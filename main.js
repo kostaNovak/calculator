d6ba951c7bb5a4c406efeb16402bda2f67e801ad
kosta011 <konstantinnovakovic7@gmail.com>
2022-10-31 21:31:05 +0100

    final logic should be working

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 function add(num1,num2){
-  return num1 + num2
+  let res = num1 + num2
+  return Math.round(res * 100) / 100
 }
 
 function subtract(num1,num2){
-  return num1 - num2
+  let res = num1 - num2
+  return Math.round(res * 100) / 100
 }
 
 function multiply(num1,num2){
-  return num1 * num2
+  let res = num1 * num2
+  return Math.round(res * 100) / 100
 }
 
 function divide(num1,num2){
-  return num1 / num2
+  let res = num1 / num2
+  return Math.round(res * 100) / 100
+
 }
 
 function operate (oper,num1,num2){
@@ -26,8 +31,8 @@ function operate (oper,num1,num2){
     return divide(num1,num2)
   }
 }
-
-
+let equalPressed = false
+let showUpper = document.getElementById('upper')
 let showVal = document.getElementById('screen')
 let expression = ''
 let operator 
@@ -35,27 +40,49 @@ let firstNumber
 let result
 
 document.addEventListener('click', function(e){
+
+  if (e.target.matches('.btnclr')){
+
+  showUpper.innerText = ''
+  showVal.innerText = ''
+  expression = ''
+  operator = undefined
+  firstNumber = undefined
+  result = undefined
+
+  }
   if (e.target.matches('.equal')){
+    if(operator != undefined && firstNumber != undefined && expression != '' ){
       result = operate(operator,+firstNumber,+expression)
+      // showUpper.innerText = result
       showVal.innerText = result
       firstNumber = result
       expression = ''
+      equalPressed = true}
 
   }
   else if(e.target.matches('.btn')){
+      
+       if (result != undefined){
+        showUpper.innerText = result
+       }
+       showVal.innerText += expression
+       showVal.innerText = e.target.value
       // operator = e.target.value 
-      showVal.innerText += e.target.value
+      
 
       if (result === undefined && firstNumber === undefined){ /// u slucaju prvog puta
+        showUpper.innerText = expression
         firstNumber = expression
         expression = ''
         operator = e.target.value
       }
-      else if(firstNumber!= '' && expression!= ''){
+      else if(firstNumber!= '' && expression!= '' ){
         result = operate(operator,+firstNumber,+expression)
         firstNumber = result
         expression =''
         operator = e.target.value
+        showUpper.innerText = result
         showVal.innerText = result
         showVal.innerText += e.target.value
       }
@@ -66,6 +93,12 @@ document.addEventListener('click', function(e){
       }
   }
   else if (e.target.matches('.numBtn')){
+    if (showVal.innerText == result){
+      showVal.innerText = ''
+      operator = undefined
+      firstNumber = undefined
+      result = undefined
+    }
     expression += e.target.value
     showVal.innerText += e.target.value
 }
